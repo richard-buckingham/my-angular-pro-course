2d656086c1eec3128234cffdcf367a9dcd517471
richie.buckingham <richie.buckingham@gmail.com>
2018-03-07 18:23:34 +1100

    project and query content child elements

--- 01-content-projection/src/app/auth-form/auth-form-component/auth-form.component.ts
+++ 01-content-projection/src/app/auth-form/auth-form-component/auth-form.component.ts
@@ -1,18 +1,31 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, ContentChild, AfterContentInit } from '@angular/core';
 
 import { User } from '../auth-form.interface';
+import { AuthRememberComponent } from '../auth-remember/auth-remember.component';
 
 @Component({
   selector: 'app-auth-form',
   templateUrl: './auth-form.component.html'
 })
-export class AuthFormComponent {
+export class AuthFormComponent implements AfterContentInit{
+
+  public showMessage: boolean = false;
+
+  // configure a content child query
+  @ContentChild(AuthRememberComponent) remember: AuthRememberComponent;
 
   @Output() submitted: EventEmitter<User> = new EventEmitter<User>();
-  constructor() { }
 
   onSubmit(value: User) {
     this.submitted.emit(value);
+  }
+
+  ngAfterContentInit() {
+    if (this.remember) {
+      // subscribe to the output event that is raised by AuthRememberComponent
+      // the the check box is checked
+      this.remember.checked.subscribe((checked: boolean) => this.showMessage = checked)
+    }
   }
 
 }
