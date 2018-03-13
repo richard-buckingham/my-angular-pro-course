b6a51e71632b5fd3fcb9ecb9bd59bc16aa90b4e9
richie.buckingham <richie.buckingham@gmail.com>
2018-03-14 08:56:11 +1100

    hostListener

--- 02-custom-attribute-directive/src/app/credit-card/credit-card.directive.ts
+++ 02-custom-attribute-directive/src/app/credit-card/credit-card.directive.ts
@@ -1,10 +1,33 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, HostListener, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[credit-card]'
 })
 export class CreditCardDirective {
-  constructor(private element: ElementRef) {
-    console.log(this.element);
+  // define an event listener for the host.
+  // the host is the element we have bound the directive to.
+  @HostListener('input', ['$event'])
+  onKeyDown(event : KeyboardEvent) {
+    console.log(event);
+    // cast input to HTMLInputElement
+    const input = event.target as HTMLInputElement;
+
+    // trim out any existing white space
+    let trimmed = input.value.replace(/\s+/g, ''); 
+
+    // trim the string to 16 char
+    if (trimmed.length > 16) {
+      trimmed = trimmed.substr(0, 16);
+    }
+
+    let numbers = [];
+    for (let i = 0; i < trimmed.length; i += 4) {
+      numbers.push(trimmed.substr(i, 4));
+    }
+    console.log(numbers);
+
+    input.value = numbers.join(' ');
+
   }
+
 }
