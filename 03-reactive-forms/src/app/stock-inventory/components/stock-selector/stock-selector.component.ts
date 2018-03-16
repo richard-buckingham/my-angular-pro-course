d9b16c140c586e2c9cb5148b26b8903ebe9e3ecd
richie.buckingham <richie.buckingham@gmail.com>
2018-03-16 17:00:01 +1100

    binding form controls

--- 03-reactive-forms/src/app/stock-inventory/components/stock-selector/stock-selector.component.ts
+++ 03-reactive-forms/src/app/stock-inventory/components/stock-selector/stock-selector.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
+import {Product} from '../../models/product.interface'
+
 @Component({
   selector: 'stock-selector',
   templateUrl: './stock-selector.component.html',
   styleUrls: ['./stock-selector.component.scss']
 })
 export class StockSelectorComponent implements OnInit {
+  
   @Input() parent: FormGroup;
+  @Input() products: Product[];
+  
   constructor() { }
 
   ngOnInit() {
+    console.log('this.products', this.products);
   }
 
 }
--- 03-reactive-forms/src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
+++ 03-reactive-forms/src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, FormArray } from '@angular/forms';
 
+import { Product } from '../../models/product.interface'
+
 @Component({
   selector: 'stock-inventory',
   styleUrls: ['stock-inventory.component.scss'],
   templateUrl: './stock-inventory.component.html'
 })
 export class StockInventoryComponent {
+
+  products: Product[] = [
+    { "id": 1, "price": 2800, "name": "MacBook Pro" },
+    { "id": 2, "price": 50, "name": "USB-C Adaptor" },
+    { "id": 3, "price": 400, "name": "iPod" },
+    { "id": 4, "price": 900, "name": "iPhone" },
+    { "id": 5, "price": 600, "name": "Apple Watch" },
+  ];
+
   form = new FormGroup({
     store: new FormGroup({
       branch: new FormControl(''),
