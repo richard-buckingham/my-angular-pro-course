58eb860375122671f83958c47d72980627aa370c
richie.buckingham <richie.buckingham@gmail.com>
2018-03-17 20:14:50 +1100

    implement the formArray

--- 03-reactive-forms/src/app/stock-inventory/components/stock-products/stock-products.component.ts
+++ 03-reactive-forms/src/app/stock-inventory/components/stock-products/stock-products.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'stock-products',
   templateUrl: './stock-products.component.html',
   styleUrls: ['./stock-products.component.scss']
 })
-export class StockProductsComponent implements OnInit {
+export class StockProductsComponent {
   @Input() parent: FormGroup;
-  constructor() { }
 
-  ngOnInit() {
+  // Get the stocks from the parent component
+  get stocks() {
+    console.log('in get Stocks...', (this.parent.get('stock') as FormArray).controls);
+    return (this.parent.get('stock') as FormArray).controls;
   }
-
 }
